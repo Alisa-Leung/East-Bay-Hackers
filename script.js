2d0f33596f0538a4b7bf97b78251a25490c56467
Alisa Leung <alisal.singyee@gmail.com>
2025-12-06 11:19:29 -0800

    upload audio

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const uploadButton = document.getElementById = "uploadButton";
-    const audioFile = document.getElementById("audioFile");
-    audioFile.addEventListener("change", (event) => {
-        const files = event.target.files;
-        if (files.length === 0){
-            return
-        }
-        })
+    const fileInput = document.getElementById("audioUpload");
+    const audioPlayer = document.getElementById("audioPlayer");
+    fileInput.addEventListener("change", (event) => {
+        uploadAndDisplayAudio(fileInput, audioPlayer);
     })
 })
+
+function uploadAndDisplayAudio(fileInput, audioPlayer){
+    const file = fileInput.files[0];
+    if (file){
+        const objectURL = URL.createObjectURL(file);
+        audioPlayer.src = objectURL;
+        audioPlayer.load();
+    }
+}
