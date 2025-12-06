cd7cbe1e8932ffc68a34c0e32b6a10396ebf2c56
Alisa Leung <alisal.singyee@gmail.com>
2025-12-06 10:37:39 -0800

    upload stuff unfinished

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const uploadButton = document.getElementById = "uploadButton";
+    const audioFile = document.getElementById("audioFile");
+    audioFile.addEventListener("change", (event) => {
+        const files = event.target.files;
+        if (files.length === 0){
+            return
+        }
+        })
+    })
+})
